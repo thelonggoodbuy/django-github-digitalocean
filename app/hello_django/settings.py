a977290a03e22ffb42fdb9c1852ad984e643ab51
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2023-09-08 21:27:47 +0300

    deploy

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '159.89.29.63']
 
 # Application definition
 
